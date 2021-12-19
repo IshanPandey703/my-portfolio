c5ac2233ebedf77d8ad3d05277a87b7bb435c41a
Ishan Pandey <pandey.ishan703@gmail.com>
2021-12-19 22:43:42 +0530

    Contact Section Responsive

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,14 +1,18 @@
-import "./Contact.css"
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Input from '@mui/material/Input';
 import TextField from '@mui/material/TextField';
 import { Button} from "@mui/material";
+import "./Contact.css"
+import gmail from "../../Media/gmail.svg";
+import github from "../../Media/github.svg";
+import linkedin from "../../Media/linkedin.svg";
+import instagram from "../../Media/instagram.svg";
 
 export default function Contact() {
 
     const InputLabelStyle = { fontFamily: "Poppins",fontWeight:"400"};
-    const InputStyle = {color:"#FFF", marginBottom:"3%",...InputLabelStyle};
+    const InputStyle = {color:"#FFF",...InputLabelStyle};
 
     return(
         <div className="Contact">
@@ -31,6 +35,20 @@ export default function Contact() {
                 <TextField label="Message" multiline maxRows = {10} fullWidth/>
                 <Button className="contact-btn" variant="outlined" > Submit </Button>
             </form>
+            <div className="connect-icons">
+                <a href="https://www.linkedin.com/in/ishan-pandey-15527717b/">
+                    <img src={linkedin} alt="linkedin icon" />
+                </a>
+                <a href="https://github.com/IshanPandey703">
+                    <img src={github} alt="github icon" />
+                </a>
+                <a href="mailto:pandey.ishan703@gmail.com">
+                    <img src={gmail} alt="gmail icon" />
+                </a>
+                <a href="https://www.instagram.com/ishan__pandey_/?hl=en">
+                    <img src={instagram} alt="instagram icon" />
+                </a>
+            </div>
         </div>
     )
 }
